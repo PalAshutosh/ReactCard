9de39f905c84a633c9b05fdcf164d58921bfcd27
PalAshutosh <palashutos397@gmail.com>
2025-10-28 08:37:55 +0530

    cards active

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Resturant from "./component/Basics/Resturant";
 
 // import UseState from './component/Hooks/useState';
 
-// import Cards from "./component/Santo_Task/Cards";
+import Cards from "./component/Santo_Task/Cards";
 
 const App = () => {
-  // return < Cards />
-  return <Resturant  />
+  return < Cards />
+  // return <Resturant  />
 }
 export default App;
